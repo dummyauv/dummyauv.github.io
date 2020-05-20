976045d7b16d4a83624da5831920614836e7293a
ShivamMalhotra1994 <shivammalhotra08@gmail.com>
2020-05-20 23:15:16 +0530

    Restructure and update readme

--- src/index.js
+++ src/index.js
@@ -25,32 +25,15 @@ import "assets/css/bootstrap.min.css";
 import "assets/scss/paper-kit.scss";
 import "assets/demo/demo.css";
 // pages
-import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
+import LandingPage from "views/LandingPage/LandingPage";
 // others
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/index" render={props => <Index {...props} />} />
-      <Route
-        path="/nucleo-icons"
-        render={props => <NucleoIcons {...props} />}
-      />
       <Route
         path="/landing-page"
         render={props => <LandingPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={props => <ProfilePage {...props} />}
-      />
-      <Route
-        path="/register-page"
-        render={props => <RegisterPage {...props} />}
       />
       <Redirect to="/landing-page" />
     </Switch>
