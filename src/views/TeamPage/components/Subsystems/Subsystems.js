64726f04035b71a3591664c6a1b606c108184be7
diya-singhal21 <diyans@iitk.ac.in>
2020-05-23 21:19:06 +0530

    made changes in team page

--- src/views/TeamPage/components/Subsystems/Subsystems.js
+++ src/views/TeamPage/components/Subsystems/Subsystems.js
@@ -32,26 +32,32 @@ function Posts() {
                     <Col md="2"></Col>
                     <Col md="6" className="text-center">
                         <h3 className="">Software</h3>
-                        <i class="nc-icon nc-laptop text-info" style={{fontSize: '200px'}}></i>
+                        <i class="nc-icon nc-laptop text-info" style={{ fontSize: '200px' }}></i>
                         <p>Remove this para and add a better laptop icon </p>
                         <p>Develop the algorithms controlling the robot - working on Computer Vision, Localisation, and Control Systems</p>
                     </Col>
                     <Col md="6" className="text-center">
                         <h3 className="">Electrical</h3>
-                        <i class="nc-icon nc-laptop text-info" style={{fontSize: '200px'}}></i>
+                        <i class="nc-icon nc-laptop text-info" style={{ fontSize: '200px' }}></i>
                         <p>Remove this para and add thunder-bolt or bulb icon</p>
                         <p>Build the electrical systems powering our AUV - working on Battery Management, Acoustic Localisation, and Circuit Designing</p>
                     </Col>
                     <Col md="6" className="text-center">
                         <h3 className=" mb-2">Mechanical</h3>
-                        <i class="nc-icon nc-laptop text-info" style={{fontSize: '200px'}}></i>
-                        <p>Remove this para and add gear icon</p>
+                        <img
+                            alt="..."
+                            src={require("assets/img/gear.jpg")}
+                            height="200px"
+                        />
                         <p>Design the entire vehicle and accessories - working on Fluid Dynamics, Actuator controls, and Pneumatics</p>
                     </Col>
                     <Col md="6" className="text-center">
                         <h3 className=" mb-2">Business</h3>
-                        <i class="nc-icon nc-laptop text-info" style={{fontSize: '200px'}}></i>
-                        <p>Remove this para and add briefcase icon</p>
+                        <img
+                            alt="..."
+                            src={require("assets/img/brief.jpg")}
+                            height="200px"
+                        />
 
                         <p>Manage the funding and outreach activities of the team by procuring sponsors and meeting people.</p>
                     </Col>
