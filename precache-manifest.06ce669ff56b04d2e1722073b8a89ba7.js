8a7e4fb4c433c052c359e559938fcf7bb12080ac
Abhishek Shree <abhi1234456@gmail.com>
2021-05-20 18:51:17 +0530

    Updates

---
+++ precache-manifest.06ce669ff56b04d2e1722073b8a89ba7.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08adaa3a68d5c9db5eada0904c262559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9549f42d605fa5359328",
+    "url": "/static/css/main.bc8af7d5.chunk.css"
+  },
+  {
+    "revision": "0c9b9dda3378610b3a67",
+    "url": "/static/js/2.3d6ee424.chunk.js"
+  },
+  {
+    "revision": "9549f42d605fa5359328",
+    "url": "/static/js/main.fd2dad2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "30e3406801a741c1010087c09f9ecb11",
+    "url": "/static/media/AnahitaElec.30e34068.jpg"
+  },
+  {
+    "revision": "9b32edad1323db054b4d0787ddc193bc",
+    "url": "/static/media/AnahitaSoft.9b32edad.jpg"
+  },
+  {
+    "revision": "ac0a0e514dba40e46cd11f27c94eede8",
+    "url": "/static/media/Krishna.ac0a0e51.jpg"
+  },
+  {
+    "revision": "4291031826da51d5dd8f52896a4c6bcf",
+    "url": "/static/media/anahita-1.42910318.jpg"
+  },
+  {
+    "revision": "301a800b84e1b64e55c9bd180af6d61c",
+    "url": "/static/media/anahita-3.301a800b.jpg"
+  },
+  {
+    "revision": "65207c6111a159b2fe5f510f9dc32f1e",
+    "url": "/static/media/anahita-underwater.65207c61.png"
+  },
+  {
+    "revision": "a3fc1e33cea7ef18fb02827320e70929",
+    "url": "/static/media/anahita_design.a3fc1e33.jpg"
+  },
+  {
+    "revision": "2672a25d9f6747f59fb90993e9ffc359",
+    "url": "/static/media/anahita_exploded.2672a25d.png"
+  },
+  {
+    "revision": "6e4bd052a238bdfe199bdb9405167af3",
+    "url": "/static/media/ansys.6e4bd052.jpg"
+  },
+  {
+    "revision": "87c14969cbea95796c20e338c30a2a2d",
+    "url": "/static/media/aryansh.87c14969.jpg"
+  },
+  {
+    "revision": "7b4a0646678a34e462734deb3a0a3617",
+    "url": "/static/media/auv_logo2.7b4a0646.png"
+  },
+  {
+    "revision": "1bc4218129f9e4f8a388fab6b69d8bba",
+    "url": "/static/media/ayush.1bc42181.jpg"
+  },
+  {
+    "revision": "55add453124c560cc097dc6a662403ab",
+    "url": "/static/media/business.55add453.jpg"
+  },
+  {
+    "revision": "83e9bf73ccba444cbb80af43a6ac5568",
+    "url": "/static/media/competitions.83e9bf73.jpg"
+  },
+  {
+    "revision": "de696a3705f2d2432ceaf74b8d7f05e4",
+    "url": "/static/media/devendra.de696a37.jpg"
+  },
+  {
+    "revision": "96a0994456f54ade0882546a0dcc77fc",
+    "url": "/static/media/dord-iitk.96a09944.png"
+  },
+  {
+    "revision": "db349aa2cfc35cc86569e7ca670d38a7",
+    "url": "/static/media/drop-header2.db349aa2.png"
+  },
+  {
+    "revision": "83a65685a0c75fd43fe83b6d33159110",
+    "url": "/static/media/electrical.83a65685.jpeg"
+  },
+  {
+    "revision": "4a62b899a50d70908764541e0eccc504",
+    "url": "/static/media/ids.4a62b899.png"
+  },
+  {
+    "revision": "cd1e30677c80893fb375e83b152d3dd9",
+    "url": "/static/media/indranil_saha.cd1e3067.jpg"
+  },
+  {
+    "revision": "b9feecca6c210b6d0109ec980a4a0530",
+    "url": "/static/media/inshu.b9feecca.jpg"
+  },
+  {
+    "revision": "e3962e4a6a52e4c6a4e32e690110ca4d",
+    "url": "/static/media/jaskeerat.e3962e4a.jpg"
+  },
+  {
+    "revision": "888ec24697b609c8974a0215d0393d40",
+    "url": "/static/media/jayant.888ec246.jpeg"
+  },
+  {
+    "revision": "ed052eb53f81df0725f33c2a6971a606",
+    "url": "/static/media/kamlesh.ed052eb5.jpg"
+  },
+  {
+    "revision": "8b5717ecc382fdf0b3ce99de7f910fc1",
+    "url": "/static/media/logo_v1.32.8b5717ec.png"
+  },
+  {
+    "revision": "3a829312467214da8d22ccc54ad8354a",
+    "url": "/static/media/mathworks.3a829312.png"
+  },
+  {
+    "revision": "f8ac6efad97bf7766b8a75d81e33bd0b",
+    "url": "/static/media/mech-paper.f8ac6efa.jpg"
+  },
+  {
+    "revision": "d11714bc0a45bf5feb72016da9ff64a3",
+    "url": "/static/media/mechanical.d11714bc.jpg"
+  },
+  {
+    "revision": "951a7ff7b23788388de90b796b0198a4",
+    "url": "/static/media/neelabh.951a7ff7.jpg"
+  },
+  {
+    "revision": "2c29b3e4d9890c58fcdad08ffbb6a609",
+    "url": "/static/media/newreplaced.2c29b3e4.jpg"
+  },
+  {
+    "revision": "21bb1f0f320322c81c4764d832729e94",
+    "url": "/static/media/niot.21bb1f0f.jpg"
+  },
+  {
+    "revision": "f192188abb43cfe670fc8d74cb5e09c2",
+    "url": "/static/media/niot.f192188a.png"
+  },
+  {
+    "revision": "2cb3f457f4f82654dfa0aaeaf50df6d8",
+    "url": "/static/media/niotwinner.2cb3f457.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "c6c6620979a68a9ddb6f8a925e60f220",
+    "url": "/static/media/p.c6c66209.jpg"
+  },
+  {
+    "revision": "5f9cd4bedd70ea49c8c07c3d5352a932",
+    "url": "/static/media/prakhar.5f9cd4be.jpg"
+  },
+  {
+    "revision": "c68ba5fd667f6e6371bc3a836fc8205b",
+    "url": "/static/media/projects.c68ba5fd.jpg"
+  },
+  {
+    "revision": "d385b347ab4783231b4bce3fd2b304b2",
+    "url": "/static/media/research.d385b347.jpg"
+  },
+  {
+    "revision": "360db1a4d8a3184f52885b0728347ec3",
+    "url": "/static/media/robosub.360db1a4.jpg"
+  },
+  {
+    "revision": "869d733b359c000cedd564ee9856aa01",
+    "url": "/static/media/robosub.869d733b.jpg"
+  },
+  {
+    "revision": "c8ac44908953ca9448d7fac20bbf6437",
+    "url": "/static/media/sajal.c8ac4490.jpg"
+  },
+  {
+    "revision": "25c7c1bc01e232307c639f24326c1f6c",
+    "url": "/static/media/sauvc.25c7c1bc.jpg"
+  },
+  {
+    "revision": "6673b8c71f1e801763e994d5f1c31f61",
+    "url": "/static/media/shashank.6673b8c7.jpeg"
+  },
+  {
+    "revision": "966bf6c6f59923b752d5132e7afd7fb4",
+    "url": "/static/media/shubham.966bf6c6.jpg"
+  },
+  {
+    "revision": "3415f3d665eb556430dacd791d4b5d91",
+    "url": "/static/media/software.3415f3d6.jpg"
+  },
+  {
+    "revision": "e94798ab50f1d9ac6ead8148387f0e92",
+    "url": "/static/media/software_logos.e94798ab.png"
+  },
+  {
+    "revision": "29b4bb51b08d57e8571f1638d15d0da5",
+    "url": "/static/media/solidworks.29b4bb51.png"
+  },
+  {
+    "revision": "6538dc34ad71e50cfe263466ec5db45e",
+    "url": "/static/media/sparton.6538dc34.png"
+  },
+  {
+    "revision": "cab12d98a10d841db581f978dfbe7e44",
+    "url": "/static/media/tanishq.cab12d98.jpg"
+  },
+  {
+    "revision": "6155038d7a2658b8a7e0f922e57f8fcd",
+    "url": "/static/media/team-photo.6155038d.jpg"
+  },
+  {
+    "revision": "649ef8aa5ab9ec5a473701e110c35778",
+    "url": "/static/media/training.649ef8aa.png"
+  },
+  {
+    "revision": "c6ddf27edb0b82db1c93c2bc6586c5f0",
+    "url": "/static/media/varun-underwater.c6ddf27e.jpg"
+  },
+  {
+    "revision": "10c9813d934d643af7efa071ed8891b1",
+    "url": "/static/media/varun_labelled.10c9813d.png"
+  },
+  {
+    "revision": "518f8d41c4602f740cb5eea61556f72c",
+    "url": "/static/media/varun_power.518f8d41.png"
+  },
+  {
+    "revision": "6b5fe5a6bdd0030a6772aa7a5b526456",
+    "url": "/static/media/xsens.6b5fe5a6.png"
+  }
+]);
